ff51de4a07b3f3466906a29e9b885b22ac2c0f05
abdallah <abdallahelsokkary4399@gmail.com>
2024-11-26 23:12:34 +0200

    before the structure of an application

--- Tut-01.fsx
+++ Tut-01.fsx
@@ -19,6 +19,7 @@ isEnable <- false
 
 //function use the same keyword as variables, because FUNCTIONS IN F# ARE VALUES
 //the last line IS the return value
+//parameters are whitespace separated and not comma as other languages
 let addFive x =
     //nested let
     let five = 5
@@ -48,4 +49,48 @@ let add' = fun x y -> x + y
 let add'' x = fun y -> x + y
 
 ////////////////////////////////////////////////////
-//Partial application
+//Partial application : you are totally gives the functions all the parameters it needs
+let add3 x =
+    let three = 5
+    x + three
+
+
+let add3' x = add x 3
+////////////////////////////////////////////////////
+
+//u can specify one parameter type with ()
+let sub (x: int) y = x - y
+
+//if you put the time at the end it's the 'return type'
+
+let sub' (x: int) y : int = x - y
+////////////////////////////////////////////////////
+//prefix and infix
+
+//this is called infix
+let add2 x = 2 + x
+//this is called prefix
+let add2' x = (+) 2 //2 + x
+////////////////////////////////////////////////////////
+//pipe operation
+//imagine we accomplish to this equation...
+let operation num = (2. * (num + 6.)) ** 2.
+//consuming the pipe operator for readability as those mathematical operation have order to be done
+let add6  = (+) 6.  //first step _following the parentheses order_
+
+let times2  = (*) 2.
+
+let pow2 num  = num ** 2.  //we can't use prefix here because it will be like : 2 ** num which violates the logic we are trying to achieve
+
+//now combine those operations using the pipe op
+
+let operation' num =
+    num
+    |> add6
+    |> times2
+    |> pow2
+    
+(*
+    as u can see they're executing sequentially e.g. if you are familiar with other OO lang it's
+    equivalent to : num.add(6).times(2).pow(2)
+*)
